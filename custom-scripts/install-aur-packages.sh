e2360a4192a80100aed30d14bb18be30d2d010c2
Zadjad Rezai <rzadjad+github@gmail.com>
2022-12-24 22:15:46 +0100

    Fix timeshift cronie issue

--- custom-scripts/install-aur-packages.sh
+++ custom-scripts/install-aur-packages.sh
@@ -6,8 +6,5 @@ set -euxo pipefail
 ## Install timeshift
 yay -S timeshift-bin
 
-## timeshift seems to have a bug where it does not create a crontab as expected
-## run this after installation and creation of your backup schedule
-# timeshift --check
-## create empty crontab
-# crontab -e
+systemctl enable cronie
+systemctl start cronie
